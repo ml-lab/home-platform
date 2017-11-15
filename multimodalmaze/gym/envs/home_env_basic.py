00c64fc0f90c8537f34f21662af463d671850e8d
Florian Golemo <fgolemo@gmail.com>
2017-11-15 18:15:13 -0500

    added basic call example

--- multimodalmaze/gym/envs/home_env_basic.py
+++ multimodalmaze/gym/envs/home_env_basic.py
@@ -6,12 +6,34 @@ import numpy as np
 class HomeEnv(gym.Env):
     """HoME basic starter environment
     TODO: explain env basics, reward, other important details
-    """
-    def __init__(self):
-        self.action_space = spaces.Box(low=np.array([-self.bounds]), high=np.array([self.bounds]))
-        self.observation_space = spaces.Discrete(4)
 
-        self.observation = None # TODO: temporary
+    Actions: (can do both move & look at the same time)
+
+        1) Moving: Discrete 5 - NOOP[0],
+                                UP[1],
+                                RIGHT[2],
+                                DOWN[3],
+                                LEFT[4]
+            - params: min: 0, max: 4
+        2) Looking: Discrete 5 -NOOP[0],
+                                UP[1],
+                                RIGHT[2],
+                                DOWN[3],
+                                LEFT[4]
+            - params: min: 0, max: 4
+
+    """
+
+    def __init__(self):
+        # "human" render creates a Tk window, returns None
+        # "rgb_array" creates NO window,
+        #             returns ndarray containing the img
+        self.metadata = {'render.modes': ['human', 'rgb_array']}
+
+        self.action_space = spaces.MultiDiscrete([[0, 4], [0, 4]])
+        self.observation_space = spaces.Discrete(1)
+
+        self.observation = 1  # TODO: temporary
 
         self._seed()
         self._reset()
@@ -23,7 +45,7 @@ class HomeEnv(gym.Env):
     def _step(self, action):
         assert self.action_space.contains(action)
 
-        pass #TODO
+        pass  # TODO
         reward = 0
         done = False
         misc = None
@@ -31,6 +53,24 @@ class HomeEnv(gym.Env):
         return self.observation, reward, done, misc
 
     def _reset(self):
-        pass # TODO
+        pass  # TODO
         return self.observation
 
+    def _render(self, mode='human', close=False):
+        pass  # TODO
+        return
+
+
+if __name__ == '__main__':
+    import multimodalmaze.gym  # to make the environment available in gym.make()
+
+    env = gym.make("Home-v0")
+    env.reset()
+
+    env.render("human")
+
+    action = env.action_space.sample()
+    print ("action:", action)
+
+    obs, rew, done, misc = env.step(action)
+    print ("obs:", obs, "rew:", rew, "done:", done, "misc:", misc)
