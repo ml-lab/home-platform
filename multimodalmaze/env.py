b8cc791249b2348d39d63ee5bd12c485a224e8b5
Florian Golemo <fgolemo@gmail.com>
2017-11-16 10:57:12 -0500

    PEP8 & bugfix

--- multimodalmaze/env.py
+++ multimodalmaze/env.py
@@ -38,113 +38,119 @@ logger = logging.getLogger(__name__)
 
 
 def extractAllRegions(occupacyGrid):
-    
     occupacyGrid = np.atleast_2d(occupacyGrid).astype(np.int)
     occupacyGrid[occupacyGrid != 0] = -1
-    
+
     curRegionId = 1
     while np.count_nonzero(occupacyGrid == 0) > 0:
-        
+
         # Apply Grassfire algorithm (also known as Wavefront or Brushfire algorithm)
         heap = []
         heapq.heapify(heap)
         visited = set()
-        
+
         # Select a start point that is not yet labelled, add the heap queue
         start = tuple(np.argwhere(occupacyGrid == 0)[0])
         heapq.heappush(heap, start)
         visited.add(start)
-        
+
         while len(heap) > 0:
             # Get cell from heap queue, assign to current region and add to visited set
-            i,j = heapq.heappop(heap)
-            occupacyGrid[i,j] = curRegionId
-            
+            i, j = heapq.heappop(heap)
+            occupacyGrid[i, j] = curRegionId
+
             # Add all 4 neighbors to heap queue, if not occupied
-            for ai,aj in [(i+1,j), (i-1,j), (i,j+1), (i,j-1)]:
-                
+            for ai, aj in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+
                 # Check bounds
                 if (ai >= 0 and ai < occupacyGrid.shape[0] and aj >= 0 and aj < occupacyGrid.shape[1]):
                     # Check occupancy and redundancy
-                    if occupacyGrid[ai,aj] == 0 and (ai,aj) not in visited:
+                    if occupacyGrid[ai, aj] == 0 and (ai, aj) not in visited:
                         heapq.heappush(heap, (ai, aj))
-                        visited.add((ai,aj))
-                        
+                        visited.add((ai, aj))
+
         curRegionId += 1
-        
+
     assert np.all(occupacyGrid != 0)
-    logger.debug('Number of regions found in occupancy map: %d' % (curRegionId-1))
-        
+    logger.debug('Number of regions found in occupancy map: %d' % (curRegionId - 1))
+
     return occupacyGrid
-        
+
+
 class BasicEnvironment(object):
-    
     def __init__(self, suncgDatasetRoot=None):
-        
+
         # Create default agent
         self.agent = Agent('agent')
-        
+
         self.renderWorld = Panda3dRenderWorld(size=(256, 256), shadowing=False, showCeiling=False)
         self.renderWorld.addDefaultLighting()
         self.renderWorld.addAgentToScene(self.agent)
-        
+
         self.physicWorld = Panda3dBulletPhysicWorld(suncgDatasetRoot)
         self.physicWorld.addAgentToScene(self.agent, radius=0.1, height=1.6, mass=60.0, mode='capsule')
-        
-        #self.acousticWorld = EvertAcousticWorld(samplingRate=16000, maximumOrder=2, materialAbsorption=False, frequencyDependent=False, showCeiling=False)
-        
-        self.worlds = [self.physicWorld, self.renderWorld]
-        
+
+        # self.acousticWorld = EvertAcousticWorld(samplingRate=16000, maximumOrder=2, materialAbsorption=False, frequencyDependent=False, showCeiling=False)
+
+        self.worlds = {
+            "physics": self.physicWorld,
+            "render": self.renderWorld
+        }
+
+        self.labeledNavMap = None
+        self.occupancyMapCoord = None
+
     def loadHouse(self, house):
-        for world in self.worlds:
+        for _, world in self.worlds.iteritems():
             world.addHouseToScene(house)
-    
+
     def generateOccupancyMap(self, minRegionArea=10.0, z=1.0, precision=0.1):
-    
-        cellArea = precision**2
-        occupancyMap, occupancyMapCoord = self.physicWorld.calculate2dNavigationMap(self.agent, z=z, precision=precision)
-        labeledNavMap = extractAllRegions(occupancyMap)
-        
+
+        cellArea = precision ** 2
+        occupancyMap, self.occupancyMapCoord = self.physicWorld.calculate2dNavigationMap(self.agent, z=z,
+                                                                                         precision=precision)
+        self.labeledNavMap = extractAllRegions(occupancyMap)
+
         # Post-processing by removing regions smaller than the threshold
         curValidRegionId = 1
-        nbRegions = int(np.max(labeledNavMap))
+        nbRegions = int(np.max(self.labeledNavMap))
         regionAreas = []
-        for r in range(1,nbRegions+1):
-            nbCells = np.count_nonzero(labeledNavMap == r)
+        for r in range(1, nbRegions + 1):
+            nbCells = np.count_nonzero(self.labeledNavMap == r)
             regionArea = nbCells * cellArea
             if regionArea < minRegionArea:
-                labeledNavMap[labeledNavMap == r] = -1
+                self.labeledNavMap[self.labeledNavMap == r] = -1
             else:
-                labeledNavMap[labeledNavMap == r] = curValidRegionId
+                self.labeledNavMap[self.labeledNavMap == r] = curValidRegionId
                 regionAreas.append(regionArea)
                 curValidRegionId += 1
-        
-        assert np.all(labeledNavMap != 0)
-        logger.debug('Number of valid regions found in occupancy map: %d' % (curValidRegionId-1))
-    
-        return labeledNavMap, occupancyMapCoord
-    
+
+        assert np.all(self.labeledNavMap != 0)
+        logger.debug('Number of valid regions found in occupancy map: %d' % (curValidRegionId - 1))
+
+        return self.labeledNavMap, self.occupancyMapCoord
+
     def generateSpawnPositions(self, n, minRegionArea=10.0, z=1.0, precision=0.1):
-        
-        labeledNavMap, occupancyMapCoord = self.generateOccupancyMap(minRegionArea, z, precision)
-        
+
+        self.generateOccupancyMap(minRegionArea, z, precision)
+
         # Calculate region areas
-        cellArea = precision**2
-        nbRegions = int(np.max(labeledNavMap))
+        cellArea = precision ** 2
+        nbRegions = int(np.max(self.labeledNavMap))
         regionAreas = []
-        for r in range(1,nbRegions+1):
-            nbCells = np.count_nonzero(labeledNavMap == r)
+        for r in range(1, nbRegions + 1):
+            nbCells = np.count_nonzero(self.labeledNavMap == r)
             regionArea = nbCells * cellArea
             regionAreas.append(regionArea)
         regionAreas = np.array(regionAreas)
-        
+
         # Calculate relative ratios between regions, and create a cummulative array
         regionAreas /= np.sum(regionAreas)
         s = np.cumsum(regionAreas)
-        
+
         positions = []
         for _ in range(n):
-        
+
             if len(s) > 1:
                 # Select a random region
                 r = np.random.random()
@@ -152,21 +158,20 @@ class BasicEnvironment(object):
                 selRegionId = idx + 1
             else:
                 selRegionId = 1
-            
+
             # Select a random cell of the region
-            coord = np.argwhere(labeledNavMap == selRegionId)
+            coord = np.argwhere(self.labeledNavMap == selRegionId)
             r = np.random.randint(low=0, high=len(coord))
-            
-            i,j = coord[r]
-            x,y = occupancyMapCoord[i,j]
-            positions.append([x,y])
-        
-        occupancyMap = np.zeros(labeledNavMap.shape, dtype=np.float)
-        occupancyMap[labeledNavMap == -1] = 1.0
-        
-        return occupancyMap, occupancyMapCoord, np.array(positions)
-        
+
+            i, j = coord[r]
+            x, y = self.occupancyMapCoord[i, j]
+            positions.append([x, y])
+
+        occupancyMap = np.zeros(self.labeledNavMap.shape, dtype=np.float)
+        occupancyMap[self.labeledNavMap == -1] = 1.0
+
+        return occupancyMap, self.occupancyMapCoord, np.array(positions)
+
     def step(self):
-        # FIXME: we should always update the physics first
-        for world in self.worlds:
-            world.step()
+        self.worlds["physics"].step()
+        self.worlds["render"].step()
